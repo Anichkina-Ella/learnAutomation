6d87a1070f3b9778f10cf7ff5ae7bbb4ba046ddb
Аничкина-Элла <Annichkinaev@ya.ru>
2021-06-14 22:44:34 +0300

    New class added (вспоминаю как заливать)

--- src/ru/lab3/ClassExample.java
+++ src/ru/lab3/ClassExample.java
@@ -3,5 +3,6 @@ package ru.lab3;
 public class ClassExample {
     public static void main(String[] args) {
         System.out.println("Hi! automation!" + 2021);
+        System.out.println("Как мне влить это на git?");
     }
 }
